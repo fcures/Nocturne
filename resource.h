0cdd92196f2d52df24af3f47eb95687a33b1307e
sevan <140277466+fcures@users.noreply.github.com>
2024-06-25 17:50:15 -0700

    Update resource.h

--- resource.h
+++ resource.h
@@ -2,3 +2,5 @@
 #define IDC_EDIT_PROJECT_NAME           1001
 #define IDC_EDIT_CANVAS_WIDTH           1002
 #define IDC_EDIT_CANVAS_HEIGHT          1003
+
+#define IDM_FILE_IMPORT                 102
